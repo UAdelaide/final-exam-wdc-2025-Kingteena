9bd5e6f60c44e12fe42d0d81712d61d1f6d29b64
Kingteena <viharag21@gmail.com>
2025-06-20 03:29:52 +0000

    [automatic commit] 2025-06-20 03:29:52 UTC ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; no servers could be reached

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -49,7 +49,7 @@ let db;
                 -- Ratings
                 INSERT INTO WalkRatings(request_id, walker_id, rating, comment)
                 VALUES
-                ((S))
+                ((SELECT request_id FROM Wa))
                 `);
     }
   } catch (err) {
