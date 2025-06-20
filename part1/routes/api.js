c9da313cf0da36221008b1abf78870e3d71b6c2b
Kingteena <viharag21@gmail.com>
2025-06-20 02:50:04 +0000

    [automatic commit] 2025-06-20 02:50:03 UTC ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; no servers could be reached

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -83,7 +83,11 @@ router.get('/dogs', async (req, res) => {
 router.get('walkrequests/open', async (req, res) => {
   try {
     const [rows] = await db.execute(`
-      SELECT WalkRequests.request_id, Dogs.name AS dog_name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, 
+      SELECT WalkRequests.request_id, Dogs.name AS dog_name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, Users.username AS owner_username
+      FROM WalkRequests
+      INNER JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id
+      INNER JOIN Users ON Dogs.owner_id = Users.user_id
+      WHERE WalkRequests.status = 'open';
       `)
   }
 });
