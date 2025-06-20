13c6347c06f16b4dd62804164c7b3a670144caa9
Kingteena <viharag21@gmail.com>
2025-06-20 03:45:10 +0000

    [automatic commit] 2025-06-20 03:45:10 UTC ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; no servers could be reached

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -111,7 +111,7 @@ router.get('/walkers/summary', async (req, res) => {
       SELECT Users.username AS walker_username,
       COUNT(WalkRatings.rating_id) AS total_ratings,
       AVG(WalkRatings.rating) AS average_rating,
-      COUNT(CASE WHEN WalkRequests.status = 'completed' THEN 1 END) AS completed
+      COUNT(CASE WHEN WalkRequests.status = 'completed' THEN 1 END) AS completed_walks
       From Users
       INNER JOIN WalkRatings ON Users.user_id = WalkRatings.walker_id
       INNER JOIN WalkRequests ON WalkRatings.request_id = WalkRequests.request_id
