d34396e40e4e10b7714dc5a09b91b7d266c782a5
Kingteena <viharag21@gmail.com>
2025-06-20 09:15:33 +0000

    [automatic commit] 2025-06-20 09:15:32 UTC ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; no servers could be reached

--- part2/app.js
+++ part2/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
+
+// Import session middleware
 const session = require('express-session');
 
 const app = express();
@@ -8,8 +10,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
-app.use(session({
-    secret: process.env.SESSION_SECRET || 'defaultsecret'}));
+app.use(session({ 'defaultsecret'}));
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
