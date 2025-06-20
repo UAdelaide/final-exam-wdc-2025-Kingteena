35a9e0d4030c1730ba911bb5dea817f5a7d3abd0
Kingteena <viharag21@gmail.com>
2025-06-20 03:28:02 +0000

    [automatic commit] 2025-06-20 03:28:01 UTC ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; no servers could be reached

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -51,7 +51,7 @@ let db;
                 VALUES
                 ((SELECT request_id FROM WalkRequests WHERE dog_id=1), (SELECT user_id FROM Users WHERE username="bobwalker"), 5, "Great walk!"),
                 ((SELECT request_id FROM WalkRequests WHERE dog_id=(SELECT dog_id FROM Dogs WHERE name="Bella") AND status="accepted"), (SELECT user_id FROM Users WHERE username="bobwalker"), 4, "Bella was a joy to walk!"),
-                ((SELECT request_id FROM WalkRequests WHERE dog_id=(SELECT dog_id FROM Dogs WHERE name="Blaze") AND status="cancelled"), (SELECT user_id FROM Users WHERE username="mal
+                ((SELECT request_id FROM WalkRequests WHERE dog_id=(SELECT dog_id FROM Dogs WHERE name="Blaze") AND status="cancelled"), (SELECT user_id FROM Users WHERE username="malfoywalker"), 3, "Blaze was a bit too energetic for me.");
             `);
     }
   } catch (err) {
