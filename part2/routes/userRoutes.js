b232aac26599f0660ac0537f6a4e1ad33d4e33f4
Kingteena <viharag21@gmail.com>
2025-06-20 09:00:51 +0000

    [automatic commit] 2025-06-20 09:00:50 UTC ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; no servers could be reached

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -48,7 +48,7 @@ router.post('/login', async (req, res) => {
     `, [username, password]);
 
     if (rows.length === 0) {
-      return res.status(401).json({ error: username, message: 'Invalid username or password', password= password });
+      return res.status(401).json({ error: 'Invalid credentials' });
     }
 
     return res.json({ message: 'Login successful', user: rows[0] });
