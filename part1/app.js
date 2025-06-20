26d7f27ccedd4dda06afd6c501a18cdc2f408895
Kingteena <viharag21@gmail.com>
2025-06-20 02:28:40 +0000

    [automatic commit] 2025-06-20 02:28:40 UTC ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; no servers could be reached

--- part1/app.js
+++ part1/app.js
@@ -7,57 +7,50 @@ var mysql = require('mysql2/promise');
 let db;
 
 (async () => {
-  try {
-    // Connect to the created database
-    db = await mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: '',
-      database: 'DogWalkService'
-    });
+    try {
+        // Connect to the created database
+        db = await mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'DogWalkService'
+        });
 
-    // Insert data if table is empty
-    const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
-    if (rows[0].count === 0) {
-      await db.execute(`
+        // Insert data if table is empty
+        const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
+        if (rows[0].count === 0) {
+            await db.execute(`
         -- Users
+        INSERT INTO Users(username, email, password_hash, role)
+        VALUES
+        ("alice123", "alice@example.com", "hashed123", "owner"),
+        ("bobwalker", "bob@example.com", "hashed456", "walker"),
+        ("carol123", "carol@example.com", "hashed789", "owner"),
+        ("randallxkcd", "randall@xkcd.com", "correcthorsebatterystaple", "owner"),
+        ("malfoywalker", "malfoy@hogwarts.com", "hashedhunter2", "walker");
 
-INSERT INTO Users(username, email, password_hash, role)
+        -- dogs
+        INSERT INTO Dogs(name, size, owner_id)
+        Values
+        ("Max", "medium", (SELECT user_id FROM Users WHERE username="alice123")),
+        ("Bella", "small", (SELECT user_id FROM Users WHERE username="carol123")),
+        ("Blaze", "medium", (SELECT user_id FROM Users WHERE username="randallxkcd")),
+        ("Alex", "large", (SELECT user_id FROM Users WHERE username="carol123")),
+        ("Teena", "medium", (SELECT user_id FROM Users WHERE username="alice123"));
 
-VALUES
-
-("alice123", "alice@example.com", "hashed123", "owner"),
-
-("bobwalker", "bob@example.com", "hashed456", "walker"),
-
-("carol123", "carol@example.com", "hashed789", "owner"),
-
-("randallxkcd", "randall@xkcd.com", "correcthorsebatterystaple", "owner"),
-
-("malfoywalker", "malfoy@hogwarts.com", "hashedhunter2", "walker");
-
--- dogs
-INSERT INTO Dogs(name, size, owner_id)
-Values
-("Max", "medium", (SELECT user_id FROM Users WHERE username="alice123")),
-("Bella", "small", (SELECT user_id FROM Users WHERE username="carol123")),
-("Blaze", "medium", (SELECT user_id FROM Users WHERE username="randallxkcd")),
-("Alex", "large", (SELECT user_id FROM Users WHERE username="carol123")),
-("Teena", "medium", (SELECT user_id FROM Users WHERE username="alice123"));
-
--- Requests
-INSERT INTO WalkRequests(dog_id, requested_time, duration_minutes, location, status)
-VALUES
-((SELECT dog_id FROM Dogs WHERE name="Max"), '2025-06-10 08:00:00', 30, "Parklands", "open"),
-((SELECT dog_id FROM Dogs WHERE name="Bella"), '2025-06-10 09:30:00', 45, "Beachside Ave", "accepted"),
-((SELECT dog_id FROM Dogs WHERE name="Blaze"), '2025-06-21 10:30:00', 300, "Adelaide Parklands", "cancelled"),
-((SELECT dog_id FROM Dogs WHERE name="Teena"), '2025-07-21 21:30:00', 15, "Unley", "open"),
-((SELECT dog_id FROM Dogs WHERE name="Bella"), '2024-06-24 15:30:00', 15, "Modburry", "completed");
+        -- Requests
+        INSERT INTO WalkRequests(dog_id, requested_time, duration_minutes, location, status)
+        VALUES
+        ((SELECT dog_id FROM Dogs WHERE name="Max"), '2025-06-10 08:00:00', 30, "Parklands", "open"),
+        ((SELECT dog_id FROM Dogs WHERE name="Bella"), '2025-06-10 09:30:00', 45, "Beachside Ave", "accepted"),
+        ((SELECT dog_id FROM Dogs WHERE name="Blaze"), '2025-06-21 10:30:00', 300, "Adelaide Parklands", "cancelled"),
+        ((SELECT dog_id FROM Dogs WHERE name="Teena"), '2025-07-21 21:30:00', 15, "Unley", "open"),
+        ((SELECT dog_id FROM Dogs WHERE name="Bella"), '2024-06-24 15:30:00', 15, "Modburry", "completed");
       `);
+        }
+    } catch (err) {
+        console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
     }
-  } catch (err) {
-    console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
-  }
 })();
 
 var indexRouter = require('./routes/index');
